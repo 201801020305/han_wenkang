8118b497d70e92f214e0a8c98c11eb2e6b0a490b
201801020305 <2237170902@qq.com>
2019-03-12 23:40:01 +0800

    重新提交的第一次作业（2.16，2.17，2.20）和第二次作业（2.18，2.19，2.23，2.24）

---
+++ HWK2.17/main.cpp
@@ -0,0 +1,17 @@
+//homework2.17----Han_Wenkang
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"12 34\n";//a)
+
+    cout<<"1"<<"2 "<<"3"<<"4\n";//b)
+
+    cout<<"1";
+    cout<<"2 ";
+    cout<<"3";
+    cout<<"4";//c)
+
+}
